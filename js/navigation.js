e5f601127003b3af1d74ca58e665ad90eecc021a
Elise Devine <elisylveon@gmail.com>
2021-12-10 16:05:11 +0000

    changed all nav bars for consistency

--- js/navigation.js
+++ js/navigation.js
@@ -2,3 +2,8 @@ function navigationToggle() {
 	let nav = document.querySelector('#navigation');
 	nav.classList.toggle('active');
 }
+
+function settingsToggle() {
+	let nav = document.querySelector('#settingslinks');
+	nav.classList.toggle('active');
+}
