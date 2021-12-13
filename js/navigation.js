020526159316d04b8a22ef9b1eac9526c1b8b6a2
Elise Devine <elisylveon@gmail.com>
2021-12-13 17:11:19 +0000

    changes to checkin process

--- js/navigation.js
+++ js/navigation.js
@@ -41,3 +41,51 @@ function goToStep4() {
 	progress.style.width = "80%";
 	progresstext.innerHTML = "80%";
 }
+
+function goToConfirmation() {
+	var step4 = document.getElementById("step4");
+	var confirmation = document.getElementById("confirmation");
+	var progress = document.getElementById("progress_bar");
+	step4.style.display = "none";
+	confirmation.style.display = "block";
+	progress_bar.style.display = "none";
+}
+
+function goToLateCheckIn() {
+	var confirmation = document.getElementById("confirmation");
+	var latecheckin = document.getElementById("latecheckin");
+	confirmation.style.display = "none";
+	latecheckin.style.display = "block";
+}
+
+//pop-ups on confirmation page
+function openPopup1() {
+	var popup1 = document.getElementById("popup1");
+	var popup2 = document.getElementById("popup2");
+	popup2.style.display = "none";
+	if (popup1.style.display === "none") {
+		popup1.style.display = "block";
+	}
+	else {
+		popup1.style.display = "none";
+	}
+}
+
+function openPopup2() {
+	var popup1 = document.getElementById("popup1");
+	var popup2 = document.getElementById("popup2");
+	popup1.style.display = "none";
+	if (popup2.style.display === "none") {
+		popup2.style.display = "block";
+	}
+	else {
+		popup2.style.display = "none";
+	}
+}
+
+function goToComplete() {
+	var latecheckin = document.getElementById("latecheckin");
+	var complete = document.getElementById("complete");
+	latecheckin.style.display = "none";
+	complete.style.display = "block";
+}
