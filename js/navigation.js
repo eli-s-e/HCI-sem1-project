c658ef58b30d94edc8e6a2829b82c5bf1f2452af
Elise Devine <elisylveon@gmail.com>
2021-12-13 12:46:48 +0000

    added to checkin process

--- js/navigation.js
+++ js/navigation.js
@@ -7,3 +7,9 @@ function settingsToggle() {
 	let nav = document.querySelector('#settingslinks');
 	nav.classList.toggle('active');
 }
+
+const mobileVerification = document.querySelector("#mobile");
+   const phoneInput = window.intlTelInput(mobileVerification, {
+     utilsScript:
+       "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+   });
